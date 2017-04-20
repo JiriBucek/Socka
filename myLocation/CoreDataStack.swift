f2f3def10d5edeeb5db2f299940a6978b10051a9
Boocha <boocha@Boocha-MacBook-Air.local>
2017-04-20 17:24:00 +0200

    pred prejmenovanim

--- myLocation/CoreDataStack.swift
+++ myLocation/CoreDataStack.swift
@@ -1,45 +1,23 @@
-
 import CoreData
 class CoreDataStack {
-    // Applications default directory address
-    lazy var applicationDocumentsDirectory: NSURL = {
-        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
-        return urls[urls.count-1]
-    }()
-    lazy var managedObjectModel: NSManagedObjectModel = {
-        // 1
-        let modelURL = NSBundle.mainBundle().URLForResource("Supercars", withExtension: "momd")!
-        return NSManagedObjectModel(contentsOfURL: modelURL)!
-    }()
-    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
-        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("SuperCars.sqlite")
-        do {
-            // If your looking for any kind of migration then here is the time to pass it to the options
-            try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
-        } catch let  error as NSError {
-            print("Ops there was an error \(error.localizedDescription)")
-            abort()
-        }
-        return coordinator
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Supercars")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error {
+                fatalError("Unresolved error \(error), \(error)")
+            }
+        })
+        return container
     }()
     
-    lazy var managedObjectContext: NSManagedObjectContext = {
-        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
-        let coordinator = self.persistentStoreCoordinator
-        var context = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
-        context.persistentStoreCoordinator = coordinator
-        return context
-    }()
-    // if there is any change in the context save it
     func saveContext() {
-        if mangagedObjectContext.hasChanges {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
             do {
-                try managedObjectContext.save()
+                try context.save()
             } catch let error as NSError {
-                print("Ops there was an error \(error.localizedDescription)")
-                abort() 
+                fatalError("Unresolved error \(error), \(error.userInfo)")
             }
-        } 
-    }
+        }
+    } 
 }
