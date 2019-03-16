d0331a175b9e9f2bddfd331d04576794b7502ade
Jiri Bucek <bucek.jiri@email.cz>
2019-03-16 17:39:33 +0100

    before pod reset

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '9.0'
+ 
+use_frameworks!
+
+target 'Socka_Watch Extension' do
+        pod 'Alamofire', '~> 4.7'
+end
+
+target 'Socka_Phone'  do
+    pod 'Alamofire', '~> 4.7'
+end
+
---
+++ Pods/Target Support Files/Pods-Socka_Watch Extension/Pods-Socka_Watch Extension-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Socka_Watch_ExtensionVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Socka_Watch_ExtensionVersionString[];
+
